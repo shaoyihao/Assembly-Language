c6d36dd19b9c559e4915b3a45572a01aef4f35bc
StaY_Hungry <74238076+shaoyihao@users.noreply.github.com>
2023-02-17 17:22:24 +0800

    Create mul.asm

---
+++ mul.asm
@@ -0,0 +1,19 @@
+assume CS:CODE, DS:DATA
+
+DATA segment
+    dw 2  ; 乘数1
+    dw 3  ; 乘数2
+    dd 0  ; 存放积的低16位
+DATA ends
+
+CODE segment
+start:
+    mov AX, DATA
+    mov DS, AX
+
+    mov AX, DS:[0]
+    mul word ptr DS:[2]  ; 乘数2存储于内存中
+    mov DS:[4], AX
+
+CODE ends
+end start
