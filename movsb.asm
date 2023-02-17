080695c544ba595e52982757f41850d88aeb5a1b
StaY_Hungry <74238076+shaoyihao@users.noreply.github.com>
2023-02-17 21:56:03 +0800

    Create movsb.asm

---
+++ movsb.asm
@@ -0,0 +1,21 @@
+assume CS:CODE, DS:DATA
+
+DATA segment
+    db 'Welcome to masm!'
+    db 16 dup (0)
+DATA ends
+
+CODE segment
+start:
+    mov AX, DATA
+    mov DS, AX
+    mov SI, 0   ; 设置 DS:SI
+    mov ES, AX
+    mov DI, 16  ; 设置 ES:DI
+
+    mov CX, 16  ; 复制16个字节
+    cld         ; 正向
+    rep movsb
+
+CODE ends
+end start
