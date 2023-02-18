8324536a69bbc26e89b4ea1e97b475b587139a1c
StaY_Hungry <74238076+shaoyihao@users.noreply.github.com>
2023-02-18 15:01:37 +0800

    Create 7_数据的移动.asm

---
+++ 7_数据的移动.asm
@@ -0,0 +1,26 @@
+; 将 ffff:0 ~ ffff:b 中的数据复制到 0:200 ~ 0:20b（等价于 0020:0 ~ 0020:b）中：
+
+assume CS:CODE
+
+CODE segment
+start:
+	
+	mov AX, 0ffffh
+	mov DS, AX
+	
+	mov AX, 20h
+	mov ES, AX
+	
+	mov BX, 0    ; 使用同一个下标
+	mov CX, 12
+	s:
+		mov DL, DS:[BX]
+		mov ES:[BX], DL
+		inc BX
+	loop s
+	
+  
+	mov AX, 4c00h
+	int 21h
+CODE ends
+end start
