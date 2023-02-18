95761acdcd553d4ad7140ffb603dd5ee0a34d3f2
StaY_Hungry <74238076+shaoyihao@users.noreply.github.com>
2023-02-18 15:42:21 +0800

    Update 7_数据的移动3.asm

--- 7_数据的移动3.asm
+++ 7_数据的移动3.asm
@@ -10,20 +10,20 @@ DATA ends
 CODE segment
 start:
 
-	mov AX, DATA
-	mov DS, AX
+    mov AX, DATA
+    mov DS, AX
 	
     mov SI, 0
 	
-	mov CX, 8
-	s:
-	    mov AX, 0[SI]    ; 0为源数组首地址
-	    mov 16[SI], AX   ; 16为目标数组首地址
-	    add SI, 2
-	loop s
+    mov CX, 8
+    s:
+        mov AX, 0[SI]    ; 0为源数组首地址
+        mov 16[SI], AX   ; 16为目标数组首地址
+        add SI, 2
+    loop s
 	
 	
-	mov AX, 4c00h
-	int 21h
+    mov AX, 4c00h
+    int 21h
 CODE ends
 end start
