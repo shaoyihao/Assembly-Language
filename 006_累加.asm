f3c0ec22649c2ea5362844acbcca9a23dc17d1d2
StaY_Hungry <74238076+shaoyihao@users.noreply.github.com>
2023-02-19 14:47:24 +0800

    Update and rename 累加.asm to 006_累加.asm

--- 006_累加.asm
+++ 006_累加.asm
@@ -1,12 +1,14 @@
+; 将 code 段中的 a 标号处的 8 个数据累加，结果存储到 b 标号处的字中。
+
 assume CS:code 
 
 code segment
     a: db 1, 2, 3, 4, 5, 6, 7, 8 
-    b: dw 0
+    b: dw 0                          ; 标号后有冒号
 
 start:
     mov SI, offset a 
-    mov BX, offset b
+    mov BX, offset b  ; 取出偏移地址
 
     mov CX, 8
     s: 
