a5db228ccfe30b2382ededcbf9fdb3828b2d0e91
StaY_Hungry <74238076+shaoyihao@users.noreply.github.com>
2023-02-19 15:27:47 +0800

    Update 018_大小写转换.asm

--- 018_大小写转换.asm
+++ 018_大小写转换.asm
@@ -13,9 +13,9 @@ start:
     mov BX, 0   ; BX用作下标
     mov CX, 5
     s:
-        mov AL, [BX]
+        mov AL, [BX]        ; 也可写为 mov AL, 0[BX]  （采用了数组的思想，0即数组的首地址）
         and AL, 11011111B
-        mov [BX], AL
+        mov [BX], AL        ; 也可写为 mov 0[BX], AL
         inc BX
     loop s
     
